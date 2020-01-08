40b241cd98a93a3c3806fc51dd3b00ac043d03b6
swsk33 <yanhanhui2001a@163.com>
2020-01-08 10:58:04 +0800

    v1.7

---
+++ src/MainTest.java
@@ -0,0 +1,8 @@
+import swsk33.RaW.*;
+public class MainTest {
+	public static void main(String[] args) throws Exception {
+		TextReader tr=new TextReader();
+		System.out.println(tr.ReadFileStart("D:\\a.txt",6));
+	}
+
+}
--- src/swsk33/RaW/TextReader.java
+++ src/swsk33/RaW/TextReader.java
@@ -13,6 +13,28 @@ public class TextReader {
 		br.close();
 		return result;
 	}
+	public String ReadFileStart(String filepath,int linestart) throws Exception {		//从文件的某一行开始读取后面所有内容并将其储存在字符串中
+		String result=null;
+		int fileline=new LineScanner().GetLinage(filepath);
+		if(linestart<=0) {
+			System.out.println("起始行数不能小于等于0！");
+		} else if(linestart>fileline) {
+			System.out.println("起始行数不能大于文档的总行数！");
+		} else {
+			File f=new File(filepath);
+			FileInputStream fis=new FileInputStream(f);
+			InputStreamReader isr=new InputStreamReader(fis);
+			BufferedReader br=new BufferedReader(isr);
+			int i;
+			for(i=0;i<linestart;i++) {
+				result=br.readLine();
+			}
+			for(int i1=i;i1<fileline;i1++) {
+				result=result+"\r\n"+br.readLine();
+			}
+		}
+		return result;
+	}
 	public String ReadFile(String filepath) throws Exception {		//读取整个文本文档并将内容储存在字符串中
 		String result=null;
 		String cdr="";
