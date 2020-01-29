f5c266eecbf906f49db94b3d71d60d8dd28d5062
swsk33 <yanhanhui2001a@163.com>
2020-01-29 20:05:53 +0800

    v2.0

--- src/swsk33/RaW/FileComparer.java
+++ src/swsk33/RaW/FileComparer.java
@@ -1,27 +1,31 @@
 package swsk33.RaW;
 import java.io.*;
 public class FileComparer {
-	public boolean comFile(String File1,String File2) throws IOException {		//比较两个文件是否完全一致
-		File f1=new File(File1);
-		File f2=new File(File2);
+	public boolean comFile(String Filepath1,String Filepath2) throws Exception {		//比较两个文件是否完全一致
 		boolean result=false;
-		BufferedReader reader1=new BufferedReader(new FileReader(File1));
-	    StringBuilder results1=new StringBuilder();
-	    String theLine1=null;
-	    while(( theLine1=reader1.readLine())!=null){
-	    	results1.append(theLine1+"\n");
-	    }
-	    BufferedReader reader2=new BufferedReader(new FileReader(File2));
-	    StringBuilder results2=new StringBuilder();
-	    String theLine2=null;
-	    while(( theLine2=reader2.readLine())!=null){
-	    	results2.append(theLine2+"\n");
-	    }
-	    String s1=results1.toString();
-	    String s2=results2.toString();
-	    if(s1.equals(s2)) {
-	    	result=true;
-	    }
+		File f1=new File(Filepath1);		//读取第一个文件
+		int fl1=new LineScanner().GetLinage(Filepath1);
+		String s1="";
+		FileInputStream fis1=new FileInputStream(f1);
+		InputStreamReader isr1=new InputStreamReader(fis1);
+		BufferedReader br1=new BufferedReader(isr1);
+		for(int i1=0;i1<fl1;i1++) {
+			s1=s1+br1.readLine()+"\r\n";
+		}
+		br1.close();
+		File f2=new File(Filepath2);		//读取第二个文件
+		int fl2=new LineScanner().GetLinage(Filepath2);
+		String s2="";
+		FileInputStream fis2=new FileInputStream(f2);
+		InputStreamReader isr2=new InputStreamReader(fis2);
+		BufferedReader br2=new BufferedReader(isr2);
+		for(int i2=0;i2<fl2;i2++) {
+			s2=s2+br2.readLine()+"\r\n";
+		}
+		br2.close();
+		if(s1.equals(s2)) {
+			result=true;
+		}
 		return result;
 	}
 }
--- src/swsk33/RaW/TextFileWriter.java
+++ src/swsk33/RaW/TextFileWriter.java
@@ -1,34 +1,41 @@
 package swsk33.RaW;
 import java.io.*;
-public class TextFileWriter {		//用指定内容替换文件指定行
-	public void replaceLine(String filepath,int whichline,String Text) throws Exception {
-        String temp="";
-        if(whichline<=new LineScanner().GetLinage(filepath)) {
-        	try {
-        		File f=new File(filepath);
-        		FileInputStream fis=new FileInputStream(f);
-        		InputStreamReader isr=new InputStreamReader(fis);
-        		BufferedReader br=new BufferedReader(isr);
-        		StringBuffer buf=new StringBuffer();
-        		for (int j=1; (temp=br.readLine())!=null;j++) {
-        			if(j==whichline){
-        				buf=buf.append(Text);
-        			}else{
-        				buf=buf.append(temp);
-        			}
-        			buf=buf.append(System.getProperty("line.separator"));
-        		}
-        		br.close();
-        		FileOutputStream fos=new FileOutputStream(f);
-        		PrintWriter pw=new PrintWriter(fos);
-        		pw.write(buf.toString().toCharArray());
-        		pw.flush();
-        		pw.close();
-        	} catch (IOException e) {
-        		e.printStackTrace();
+public class TextFileWriter {		
+	public void replaceLine(String filepath,int whichline,String Text) throws Exception {		//用指定内容替换文件指定行
+		int fl=new LineScanner().GetLinage(filepath);
+        if(0<whichline&&whichline<=fl) {
+        	File f=new File(filepath);
+        	String sumstr;
+        	String front="";
+        	FileInputStream fisf=new FileInputStream(f);
+        	InputStreamReader isrf=new InputStreamReader(fisf);
+        	BufferedReader brf=new BufferedReader(isrf);
+        	for(int ifr=0;ifr<whichline-1;ifr++) {
+        		front=front+brf.readLine()+"\r\n";
         	}
-        }else if(whichline>new LineScanner().GetLinage(filepath)){
+        	sumstr=front+Text+"\r\n";
+        	brf.close();
+        	String after="";
+        	FileInputStream fisa=new FileInputStream(f);
+        	InputStreamReader isra=new InputStreamReader(fisa);
+        	BufferedReader bra=new BufferedReader(isra);
+        	for(int ia1=0;ia1<whichline;ia1++) {
+        		bra.readLine();
+        	}
+        	for(int ia2=0;ia2<fl-whichline;ia2++) {
+        		after=after+bra.readLine()+"\r\n";
+        	}
+        	sumstr=sumstr+after;
+        	bra.close();
+        	FileOutputStream fos=new FileOutputStream(f);
+        	OutputStreamWriter osw=new OutputStreamWriter(fos);
+        	BufferedWriter bw=new BufferedWriter(osw);
+        	bw.write(sumstr);
+        	bw.close();
+        }else if(whichline>fl){
         	System.out.println("错误！超过文件最大行数！");
+        }else if(whichline<=0) {
+        	System.out.println("错误！指定行数不可小于等于0！");
         }
 	}
 	public void writeText(String filepath,String Text) throws Exception {		//写入指定内容至文件
