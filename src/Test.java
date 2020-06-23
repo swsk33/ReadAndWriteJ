9d4821b588a1596643b9665623acd3e29856fd9d
swsk33 <yanhanhui2001a@163.com>
2020-06-23 14:22:16 +0800

    v3.0

--- src/Test.java
+++
@@ -1,8 +0,0 @@
-import swsk33.RaW.*;
-
-public class Test {
-	public static void main(String[] args) throws Exception {
-		TextFileWriter tfr=new TextFileWriter();
-		tfr.replaceLine("D:\\Documents\\Downloads\\a.txt", 1, "阿诗丹顿多多多多多多多多多所2a ");
-	}
-}
