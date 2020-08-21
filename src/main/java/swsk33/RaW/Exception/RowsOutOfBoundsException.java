a4572bd15d009ff7217ef0c70a32261732d04a49
swsk33 <yanhanhui2001a@163.com>
2020-08-21 23:26:16 +0800

    v4.0.0

--- src/main/java/swsk33/RaW/Exception/RowsOutOfBoundsException.java
+++ src/main/java/swsk33/RaW/Exception/RowsOutOfBoundsException.java
@@ -10,7 +10,7 @@ public class RowsOutOfBoundsException extends Exception {
 	/**
 	 * 行数越界异常，当读取文件指定的行数超过其本有的行数时抛出此异常
 	 * 
-	 * @param msg
+	 * @param msg 抛出异常的信息
 	 */
 	public RowsOutOfBoundsException(String msg) {
 		super(msg);
